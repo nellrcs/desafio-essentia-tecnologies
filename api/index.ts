24851a504516b8d68f3aaef39eebe5a458589b5e
nellrcs <warllencs@gmail.com>
2026-05-21 22:51:36 -0300

    correcao erro cors

--- api/index.ts
+++ api/index.ts
@@ -4,7 +4,17 @@ import cors from 'cors';
 const app = express();
 const PORT = process.env.PORT || 8083;
 
-app.use(cors());
+//app.use(cors());
+
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  optionsSuccessStatus: 200
+}));
+
+app.options('*', cors()); 
+
 app.use(express.json());
 
 interface Tarefa {
